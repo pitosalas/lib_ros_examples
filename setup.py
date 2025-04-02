aa99a771245fd71763307294bcd9744bf6658070
Pito Salas <pitosalas@users.noreply.github.com>
2025-04-02 11:33:07 -0400

    update

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'lib_ros_examples'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='Example ROS 2 package for driving in a square and circle.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'square_drive = lib_ros_examples.src.square_drive:main',
+        ],
+    },
+)
